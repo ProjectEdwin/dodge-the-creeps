2c252c64629b10cc9b6881fd319b7cf0df440695
Edwin Soto <edwin@edwinsoto.net>
2026-02-07 13:28:18 -0800

    Gradual difficulty
    * Added mob gradual velocity difficulty
    * Added gradual mob frequency
    * Made Player and Mobs Smaller
    * Added Mob sounds

--- hud.gd
+++ hud.gd
@@ -3,10 +3,12 @@ extends CanvasLayer
 # Notifies `Main` node that the button has been pressed
 signal start_game
 
+
 func show_message(text):
 	$Message.text = text
 	$Message.show()
 	$MessageTimer.start()
+
 
 func show_game_over(message):
 	show_message(message)
@@ -19,15 +21,19 @@ func show_game_over(message):
 	await get_tree().create_timer(1.0).timeout
 	$StartButton.show()
 
+
 func update_score(score):
 	$ScoreLabel.text = str(score)
+
 	
 func update_hi_score(high_score):
 	$HiScoreLabel.text = "Best: " + str(high_score)
 
+
 func _on_start_button_pressed() -> void:
 	$StartButton.hide()
 	start_game.emit()
+
 
 func _on_message_timer_timeout() -> void:
 	$Message.hide()
--- main.gd
+++ main.gd
@@ -1,26 +1,18 @@
 extends Node
+## Main Game Logic
+## Adapted from Dodge the Creeps Godot tutorial
 
 @export var mob_scene: PackedScene
+
 var score
 var hi_score = 0
+var velocity
+
 
 func _ready():
 	$Player.hide()
 	$HUD.update_hi_score(hi_score)
 
-func game_over() -> void:
-	$ScoreTimer.stop()
-	$MobTimer.stop()
-	$Music.stop()
-	
-	if score > hi_score:
-		hi_score = score
-		$HUD.update_hi_score(hi_score)
-		$HiScoreSound.play()
-		$HUD.show_game_over("New Hi Score!")
-	else:
-		$HUD.show_game_over("Game Over")
-		$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -31,7 +23,21 @@ func new_game():
 	get_tree().call_group("mobs", "queue_free")
 	$Music.play()
 
-func _on_mob_timer_timeout() -> void:
+
+func set_mob_velocity():
+	var multiplier = score * 2
+	velocity = Vector2(randf_range(100.0 + multiplier, 150.0 + multiplier), 0.0)
+	print(velocity)
+
+	
+func set_mob_frequency():
+	#decrease the MobTimer every 5 points
+	if score % 5 == 0:
+		$MobTimer.wait_time -= 0.1
+	print($MobTimer.wait_time)
+
+
+func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
 
@@ -50,17 +56,35 @@ func _on_mob_timer_timeout() -> void:
 	mob.rotation = direction
 
 	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	set_mob_velocity()
 	mob.linear_velocity = velocity.rotated(direction)
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
+	$MobSound.play()
+
 
 func _on_score_timer_timeout():
 	score += 1
 	$HUD.update_score(score)
+	set_mob_frequency()
 
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$Music.stop()
+	
+	if score > hi_score:
+		hi_score = score
+		$HUD.update_hi_score(hi_score)
+		$HiScoreSound.play()
+		$HUD.show_game_over("New Hi Score!")
+	else:
+		$HUD.show_game_over("Game Over")
+		$DeathSound.play()
