7d5f765fa1445fe61e32c71b50cd1fcd06b685c4
Edwin Soto <edwin@edwinsoto.net>
2026-02-10 17:59:13 -0800

    Added Tiny Power Up
    * Logic to select powerups
    * New sound effects by yours truly for tiny and reset of the Player.
    * Tiny power up added to PowerUp scene
    * Added layers and masks for collision detection as player, enemy, power_ups

--- player.gd
+++ player.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
 signal hit
+signal power_up_taken
+var PowerSETiny = preload("res://sound/tiny.wav")
+var PowerSEReset = preload("res://sound/reset.wav") 
+
+#TODO: Change to be dynamically load sounds as ENUMS?
 
 
 @export var speed = 200 # How fast the player will move (pixels/sec).
@@ -38,18 +43,43 @@ func _process(delta: float) -> void:
 		$AnimatedSprite2D.flip_v = false
 		# See the note below about the following boolean assignment.
 		$AnimatedSprite2D.flip_h = velocity.x < 0
+	
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 
 
 func _on_body_entered(_body):
-	hide() # Player disappears after being hit.
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
-	
-	
+	if _body.collision_layer == 2:
+		hide() # Player disappears after being hit.
+		hit.emit()
+		# Must be deferred as we can't change physics properties on a physics callback.
+		$CollisionShape2D.set_deferred("disabled", true)
+
+	elif _body.collision_layer == 4:
+		power_up_taken.emit()
+		var type = _body.type
+		power_up_mode(type)
+		print("!!" + type + " power up mode activated!!")
+
+
+func power_up_mode(mode):
+	if mode == "Tiny":
+		$AnimatedSprite2D.scale = Vector2(0.25, 0.25)
+		$CollisionShape2D.scale = Vector2(0.25, 0.25)
+		speed = 350
+		
+		$PlayerSound.stream = PowerSETiny
+		$PlayerSound.play()
+
+
+func reset_power_up():
+		$AnimatedSprite2D.scale = Vector2(0.5, 0.5)
+		$CollisionShape2D.scale = Vector2(0.5, 0.5)
+		speed = 200
+		$PlayerSound.stream = PowerSEReset
+		$PlayerSound.play()
+
 func start(pos):
 	position = pos
 	show()
